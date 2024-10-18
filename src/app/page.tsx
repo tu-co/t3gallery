254ccc0b3e000df2b250519e482d31c8381c8791
tu-co <tu.co.5.dev@gmail.com>
2024-10-17 19:16:53 -0600

    render post data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,13 +22,17 @@ export default async function HomePage() {
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
+        {posts.map((post) => (
+          <div key={post.id} className="w-48">
+            <p>{post.name}</p>
+          </div>
+        ))}
         {mockImages.map((image) => (
           <div key={image.id} className="w-48">
             <img src={image.url} alt="image" />
           </div>
         ))}
       </div>
-      Hello (gallery in progress)
     </main>
   );
 }
