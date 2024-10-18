394db1fd1b7c51c5f350b64ee911ef343fa7762f
tu-co <tu.co.5.dev@gmail.com>
2024-10-17 23:13:22 -0600

    auth home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,43 @@
 import { db } from "@/server/db";
+import { SignedIn, SignedOut } from "@clerk/nextjs";
+import Image from "next/image";
 
 export const dynamic = "force-dynamic";
 
-export default async function HomePage() {
+async function Images() {
   const images = await db.query.images.findMany({
     orderBy: (model, { desc }) => desc(model.id),
   });
 
   return (
+    <div className="flex flex-wrap items-stretch gap-4">
+      {images.map((image) => (
+        <div key={image.id} className="flex flex-col items-center">
+          <Image
+            src={image.url}
+            width={192}
+            height={128}
+            alt="image"
+            className="h-32 w-48 object-cover"
+          />
+          <div>{image.name}</div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default function HomePage() {
+  return (
     <main className="">
-      <div className="flex flex-wrap gap-4">
-        {images.map((image) => (
-          <div key={image.id} className="flex w-48 flex-col">
-            <img src={image.url} alt="image" />
-            <div>{image.name}</div>
-          </div>
-        ))}
-      </div>
+      <SignedOut>
+        <div className="h-full w-full text-center text-2xl">
+          Please sign in above
+        </div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
